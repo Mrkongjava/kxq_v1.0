26dca163e167308aea5517e22b306b54d850cd25
kxq <15622206096@163.com>
2019-11-13 17:16:34 +0800

    优化pom.xml，排除没必要进行打包的jar包

--- demo1/src/main/java/com/group/demo1/timer/TimerTest.java
+++ demo1/src/main/java/com/group/demo1/timer/TimerTest.java
@@ -11,16 +11,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class TimerTest {
 
-    @Scheduled(fixedDelay = 5000)
+    @Scheduled(fixedDelay = 5000)//一秒执行一次
     public void updateOrder() throws InterruptedException {
         System.out.println("定时任务执行1----------------------" + Thread.currentThread().getName());
         Thread.sleep(2000);
         System.out.println("定时任务1执行完毕");
     }
 
-    @Scheduled(fixedDelay = 1000)
-    public void updateOrder2() throws InterruptedException {
-        System.out.println("定时任务执行2----------------------" + Thread.currentThread().getName());
-        System.out.println("定时任务2执行完毕");
-    }
+//    @Scheduled(fixedDelay = 1000)
+//    public void updateOrder2() throws InterruptedException {
+//        System.out.println("定时任务执行2----------------------" + Thread.currentThread().getName());
+//        System.out.println("定时任务2执行完毕");
+//    }
 }
